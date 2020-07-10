21d92ed1f64dd852036833012518012d22180806
Ada <asmelliem@gmail.com>
2020-07-09 20:17:08 -0400

    Added code to calculate room area and perimeter

--- RoomDetailGenerator/Program.cs
+++ RoomDetailGenerator/Program.cs
@@ -6,7 +6,36 @@ namespace RoomDetailGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool proceed = false;
+            do
+            {
+                Console.WriteLine("Enter Length: ");
+                var lengthInput = Console.ReadLine();
+                Console.WriteLine("Enter Width: ");
+                var widthInput = Console.ReadLine();
+
+                bool lengthSuccess = float.TryParse(lengthInput, out float length);
+                bool widthSuccess = float.TryParse(widthInput, out float width);
+
+                float area = length * width;
+                float perimeter = 2 * (length + width);
+
+                Console.WriteLine($"Area: {area}");
+                Console.WriteLine($"Perimeter: {perimeter}");
+                Console.WriteLine("");
+                Console.WriteLine("Continue? (y/n)");
+                string cont = Console.ReadLine().ToUpper();
+
+                if (cont == "Y")
+                {
+                    proceed = true;
+                    Console.WriteLine("");
+                }
+                else
+                {
+                    proceed = false;
+                }
+            } while (proceed == true);
         }
     }
 }
