bdc0ba248a5c044721d1f1179a7794073e4d48ba
Ada <asmelliem@gmail.com>
2020-07-09 22:16:11 -0400

    Added calculation for room volume and error checking for non-numeric inputs

--- RoomDetailGenerator/Program.cs
+++ RoomDetailGenerator/Program.cs
@@ -6,35 +6,49 @@ namespace RoomDetailGenerator
     {
         static void Main(string[] args)
         {
-            bool proceed = false;
+            bool proceed;
             do
             {
                 Console.WriteLine("Enter Length: ");
                 var lengthInput = Console.ReadLine();
                 Console.WriteLine("Enter Width: ");
                 var widthInput = Console.ReadLine();
+                Console.WriteLine("Enter Height: ");
+                var heightInput = Console.ReadLine();
 
                 bool lengthSuccess = float.TryParse(lengthInput, out float length);
                 bool widthSuccess = float.TryParse(widthInput, out float width);
+                bool heightSuccess = float.TryParse(heightInput, out float height);
 
-                float area = length * width;
-                float perimeter = 2 * (length + width);
-
-                Console.WriteLine($"Area: {area}");
-                Console.WriteLine($"Perimeter: {perimeter}");
-                Console.WriteLine("");
-                Console.WriteLine("Continue? (y/n)");
-                string cont = Console.ReadLine().ToUpper();
-
-                if (cont == "Y")
+                if (!lengthSuccess || !widthSuccess || !heightSuccess)
                 {
-                    proceed = true;
+                    Console.WriteLine("One of your inputs was invalid. Please try again.");
                     Console.WriteLine("");
+                    proceed = true;
                 }
                 else
                 {
-                    proceed = false;
-                }
+                    float area = length * width;
+                    float perimeter = 2 * (length + width);
+                    float volume = length * width * height;
+
+                    Console.WriteLine($"Area: {area} sqft.");
+                    Console.WriteLine($"Perimeter: {perimeter} ft.");
+                    Console.WriteLine($"Volume: {volume} cubic ft.");
+                    Console.WriteLine("");
+                    Console.WriteLine("Continue? (y/n)");
+                    string cont = Console.ReadLine().ToUpper();
+
+                    if (cont == "Y")
+                    {
+                        proceed = true;
+                        Console.WriteLine("");
+                    }
+                    else
+                    {
+                        proceed = false;
+                    }
+                }                
             } while (proceed == true);
         }
     }
